c0be96d08a06de0b1f47c3b2e8637fc6f242643f
Radu Cotescu <cotescu@adobe.com>
2014-11-03 11:18:14 +0200

    fix #27: NativeFileSystem module is deprecated
    * removed references to NFS and updated module to use Brackets new file system APIs

--- sly/Preview.js
+++ sly/Preview.js
@@ -6,84 +6,87 @@
  ******************************************************************************/
 
 /*
-    Preview does add a button for previewing sly code (it generates html source in sly-preview directory, and uses Brackets HTML Preview on it)
-*/
+ Preview does add a button for previewing sly code (it generates html source in sly-preview directory, and uses Brackets HTML Preview on it)
+ */
 
 
 /*  These are jslint options. Using linters is recommended, but optional */
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets */
 
-define(function (require, exports, module) {    
+define(function (require, exports, module) {
     'use strict';
 
     // Configuration
-    var shortcut            = "Ctrl-Shift-T";
-    var commandId           = "npeltier.SLYPreview";
-    var commandName         = "SLY Preview";
-    var buttonColors       = {"off":"#cccccc", "on":"#4084CE"};
-    
+    var shortcut = "Ctrl-Shift-T";
+    var commandId = "npeltier.SLYPreview";
+    var commandName = "SLY Preview";
+    var buttonColors = {"off": "#cccccc", "on": "#4084CE"};
+
     // Load submodules
 
     // Load dependent modules
-    var Commands            = brackets.getModule("command/Commands"),
-        CommandManager      = brackets.getModule("command/CommandManager"),
-        DocumentManager     = brackets.getModule("document/DocumentManager"),
-        Inspector           = brackets.getModule("LiveDevelopment/Inspector/Inspector"),
-        LiveDevelopment     = brackets.getModule("LiveDevelopment/LiveDevelopment"),
-        KeyBindingManager   = brackets.getModule("command/KeyBindingManager"),
-        ProjectManager      = brackets.getModule("project/ProjectManager"),
-        NativeFileSystem    = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
-        FileUtils           = brackets.getModule("file/FileUtils");
+    var Commands = brackets.getModule("command/Commands"),
+        CommandManager = brackets.getModule("command/CommandManager"),
+        DocumentManager = brackets.getModule("document/DocumentManager"),
+        Inspector = brackets.getModule("LiveDevelopment/Inspector/Inspector"),
+        LiveDevelopment = brackets.getModule("LiveDevelopment/LiveDevelopment"),
+        KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
+        ProjectManager = brackets.getModule("project/ProjectManager"),
+        FileSystem = brackets.getModule("filesystem/FileSystem"),
+        FileUtils = brackets.getModule("file/FileUtils"),
+        Directory = brackets.getModule('filesystem/Directory');
 
     // State
     var $slyButton;
-    
-    function generate(src, destination) {
-        var deferred = new $.Deferred(); 
+
+    function generate(src, destination) {
+        var deferred = new $.Deferred();
         FileUtils.readAsText(src)
-            .done(function(text){
+            .done(function (text) {
                 FileUtils.writeText(destination, text)
-                    .done(function(){
+                    .done(function () {
                         deferred.resolve();
                     })
-                    .fail(function(error){
+                    .fail(function (error) {
                         console.error(error);
-                    });  
+                    });
             })
-            .fail(function(error){
+            .fail(function (error) {
                 console.error(error);
             });
         return deferred.promise();
     }
-    
+
     /**
-      * Create preview directory, returns its path
-      */
-    function createPreviewDirectory(){
+     * Create preview directory, returns its path
+     */
+    function createPreviewDirectory() {
         var root = ProjectManager.getProjectRoot(),
             folderName = "sly-preview",
-            deferred = new $.Deferred();
-        root.getDirectory(folderName, {"create":true,"exclusive":false},
-            function(folderEntry){
-                deferred.resolve(folderEntry);
-            },
-            function(errorEntry){
-                deferred.reject(errorEntry);
-            });  
+            deferred = new $.Deferred(),
+            directory;
+        directory = FileSystem.getDirectoryForPath(root.fullPath + folderName);
+        directory.create(function (err, stats) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(directory);
+            }
+        });
         return deferred.promise();
     }
-    
+
     /**
-      * Indicates wether preview is on
-      */    
+     * Indicates wether preview is on
+     */
     function isSLYPreviewOn() {
         return $slyButton.status === "on";
     }
-    
+
     /**
-      * Enable / Disable the preview (and refresh button state) 
-      */
+     * Enable / Disable the preview (and refresh button state)
+     */
     function toggleSLYPreviewStatus() {
         $slyButton.status = isSLYPreviewOn() ? "off" : "on";
         $(".sly-preview-button").css({color: buttonColors[$slyButton.status]});
@@ -93,40 +96,38 @@ define(function (require, exports, module) {
      * starts the sly preview
      */
     function SLYPreview() {
-        if (isSLYPreviewOn()){
+        if (isSLYPreviewOn()) {
             LiveDevelopment.close();
             toggleSLYPreviewStatus();
         } else {
             var document = DocumentManager.getCurrentDocument();
-            if (document){
+            if (document) {
                 createPreviewDirectory()
-                .done(function(directory){
-                    var fileName =  document.file.name,
-                        newFilePath =  directory.fullPath + fileName;                 
-                    directory.getFile(fileName, {"create":true, "exclusive":false},
-                        function(fileEntry){
-                            generate(document.file, fileEntry);
-                            DocumentManager.getDocumentForPath(fileEntry.fullPath).done(function(doc){
-                                DocumentManager.setCurrentDocument(doc);                            
-                                LiveDevelopment.open();   
-                                toggleSLYPreviewStatus();
-                            });
-                        }, function(error) {
-                                console.error(error);    
-                            });
-    
-                })
-                .fail(function(error){
-                    console.error(error);        
-                });
+                    .done(function (directory) {
+                        var fileName = document.file.name,
+                            file;
+                        file = FileSystem.getFileForPath(directory.fullPath + fileName);
+                        generate(document.file, file).then(
+                            function () {
+                                DocumentManager.getDocumentForPath(file.fullPath).done(function (doc) {
+                                    DocumentManager.setCurrentDocument(doc);
+                                    LiveDevelopment.open();
+                                    toggleSLYPreviewStatus();
+                                });
+                            }
+                        ).done();
+
+                    }).fail(function (error) {
+                        console.error(error);
+                    });
             }
         }
     }
-        
-    
+
+
     /* 
-        adds the SLY button
-    */
+     adds the SLY button
+     */
     function addSLYButton() {
         // Insert the sly button in the toolbar to the left of the first a element (life preview button)
         $slyButton = $("<a>")
@@ -135,16 +136,16 @@ define(function (require, exports, module) {
             .addClass("sly-preview-button")
             .click(SLYPreview)
             .css({
-                "margin-right":     "10px",
-                "font-weight":      "bold",
-                "color":            buttonColors.off
+                "margin-right": "10px",
+                "font-weight": "bold",
+                "color": buttonColors.off
             })
             .insertAfter("#main-toolbar .buttons a:last");
     }
 
     /*
-        removes the SLY Button
-    */
+     removes the SLY Button
+     */
     function removeSLYButton() {
         $slyButton.remove();
     }
@@ -158,7 +159,7 @@ define(function (require, exports, module) {
 
         addSLYButton();
     }
-    
+
     function unload() {
         KeyBindingManager.removeBinding(shortcut);
         // Not possible
